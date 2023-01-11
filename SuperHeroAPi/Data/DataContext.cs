0fe3a686dce13bf8b72e2ef1e43f4cb47c3744e8
bartfai-balint <79147031+bartfai-balint@users.noreply.github.com>
2023-01-11 12:44:58 +0100

    Add project files.

---
+++ SuperHeroAPi/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SuperHeroAPi.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+
+    }
+}
---
+++ SuperHeroAPi/SuperHero.cs
@@ -0,0 +1,13 @@
+﻿namespace SuperHeroAPi
+{
+    public class SuperHero
+    {
+        public int Id { get; set; }
+
+        public String Name { get; set; } = string.Empty;
+
+        public String FirstName { get; set; } = string.Empty;
+        public String LastName { get; set; } = string.Empty;
+        public String Place { get; set; } = string.Empty;
+    }
+}
